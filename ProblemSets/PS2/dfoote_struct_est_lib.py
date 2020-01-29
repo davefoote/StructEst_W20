a4a70dfa55e5437e90282be9ac07dbc7a3d3599c
davefoote <dfoote@uchicago.edu>
2020-01-29 13:29:04 -0600

    not late?

--- ProblemSets/PS2/dfoote_struct_est_lib.py
+++ ProblemSets/PS2/dfoote_struct_est_lib.py
@@ -4,6 +4,8 @@ import scipy.stats as sts
 from matplotlib import pyplot as plt
 import requests
 import scipy.optimize as opt
+import scipy.special as spc
+import math
 
 '''
 Dave Foote's Structural Estimation Library
@@ -37,7 +39,7 @@ def histogram(data, num_bins, title, xlab, ylab, xlim, og_data=np.array([])):
     if og_data.any():
         wt = (1/og_data.shape[0]) * np.ones_like(data)
         count, bins, ignored = plt.hist(data, num_bins,
-                                    edgecolor='k', weights=wt)
+                                    edgecolor='k', density=True)
     else:
         count, bins, ignored = plt.hist(data, num_bins, density=True,
                                     edgecolor='k')
@@ -46,6 +48,8 @@ def histogram(data, num_bins, title, xlab, ylab, xlim, og_data=np.array([])):
     plt.xlabel(xlab)
     plt.ylabel(ylab)
     plt.xlim(xlim)
+    
+    return count
     
 '''
 mle functions
@@ -66,6 +70,14 @@ def log_lik(x, params, f):
         p2 = params[2]
 
         return np.log(f(x, p0, p1, p2)).sum()
+        
+    if len(params) == 4:
+        p0 = params[0]
+        p1 = params[1]
+        p2 = params[2]
+        p3 = params[3]
+
+        return np.log(f(x, p0, p1, p2, p3)).sum()
 
 def mle(guess, f, data):
     '''
@@ -97,6 +109,19 @@ def mle3(guess, f, data):
     
     return p1, p2, p3
 
+def mle4(guess, f, data):
+    p1_i = guess[0]
+    p2_i = guess[1]
+    p3_i = guess[2]
+    p4_i = guess[3]
+    p_init = np.array([p1_i, p2_i, p3_i, p4_i])
+    data[data < 1e-10] = 1e-10
+    results_uncstr = opt.minimize(f, p_init, args=(data))
+    p1, p2, p3, p4 = results_uncstr.x
+    print(results_uncstr)
+    
+    return p1, p2, p3, p4
+
 '''
 gamma functions
 '''
@@ -121,10 +146,12 @@ def make_gamma_pdf(x, alpha, beta):
     generates pdfs values from a gamma distribution fitting specifiedx, alpha,
     and beta
     '''
-    vals = sts.gamma.pdf(x, alpha, scale=beta)
-    vals[vals < 1e-10] = 1e-10
+    x[x < 1e-10] = 1e-10
+    rv = ((1 / ((beta ** alpha) * spc.gamma(alpha))) *
+                (x ** (alpha - 1)) * (np.exp(-x/beta)))
+    
 
-    return vals
+    return rv
 
 def gamma_crit(params, data):
     '''
@@ -139,6 +166,8 @@ def gamma_crit(params, data):
     pdf_function = the function you would like to use for pdf generating
     data = (N,) vector, values of the gamma distributed random variable
     '''
+    print(data.shape)
+    print(params)
     log_lik_value = log_lik(data, params, make_gamma_pdf)
 
     return -log_lik_value
@@ -202,17 +231,48 @@ def gb2_firstguess(gamma_output):
 
 def make_gb2_pdf(x, params):
     '''
-    generates pdfs values from a gamma distribution fitting specifiedx, alpha,
-    and beta
-    '''
-    a = params[0]
-    b = params[1]
-    p = params[2]
-    q = params[3]
-    vals = sts.beta.pdf()
-    vals[vals < 1e-10] = 1e-10
+        --------------------------------------------------------------------
+    Returns the PDF values from the four-parameter generalized beta 2
+    (GB2) distribution. See McDonald and Xu (1995).
 
-    return vals
+    (GB2): f(x; a, b, p, q) = (a * (x ** ((a*p) - 1))) /
+        ((b ** (a * p)) * spc.beta(p, q) *
+        ((1 + ((x / b) ** a)) ** (p + q)))
+    x in [0, infty), alpha, beta, m > 0
+    --------------------------------------------------------------------
+    INPUTS:
+    xvals = (N,) vector, values in the support of generalized beta 2
+            (GB2) distribution
+    params = tuple, containing the following:
+    aa    = scalar > 0, generalized beta 2 (GB2) distribution parameter
+    bb    = scalar > 0, generalized beta 2 (GB2) distribution parameter
+    pp    = scalar > 0, generalized beta 2 (GB2) distribution parameter
+    qq    = scalar > 0, generalized beta 2 (GB2) distribution parameter
+
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
+        spc.beta()
+
+    OBJECTS CREATED WITHIN FUNCTION:
+    pdf_vals = (N,) vector, pdf values from generalized beta 2 (GB2)
+               distribution corresponding to xvals given parameters aa,
+               bb, pp, and qq
+
+    FILES CREATED BY THIS FUNCTION: None
+
+    RETURNS: pdf_vals
+    --------------------------------------------------------------------
+    '''
+    aa = params[0]
+    bb = params[1]
+    pp = params[2]
+    qq = params[3]
+    x[x < 1e-10] = 1e-10
+
+    return np.float64((aa * (x ** (aa * pp - 1))) / np.exp((spc.logsumexp(bb) * (spc.logsumexp(aa * pp)
+                                                    + spc.logsumexp(spc.beta(pp, qq)))
+                                                    + spc.logsumexp(1 + ((x / bb) ** aa) ** 
+                                                       (pp + qq)))))
+
 
 def gb2_crit(params, data):
     '''
@@ -232,3 +292,48 @@ def gb2_crit(params, data):
     log_lik_value = log_lik(data, params, make_gb2_pdf)
 
     return -log_lik_value
+
+def crit(func, params, data):
+    return -log_lik(data, params, func)
+
+'''
+from prof
+'''
+def GB2_pdf(xvals, aa, bb, pp, qq):
+    '''
+    --------------------------------------------------------------------
+    Returns the PDF values from the four-parameter generalized beta 2
+    (GB2) distribution. See McDonald and Xu (1995).
+
+    (GB2): f(x; a, b, p, q) = (a * (x ** ((a*p) - 1))) /
+        ((b ** (a * p)) * spc.beta(p, q) *
+        ((1 + ((x / b) ** a)) ** (p + q)))
+    x in [0, infty), alpha, beta, m > 0
+    --------------------------------------------------------------------
+    INPUTS:
+    xvals = (N,) vector, values in the support of generalized beta 2
+            (GB2) distribution
+    aa    = scalar > 0, generalized beta 2 (GB2) distribution parameter
+    bb    = scalar > 0, generalized beta 2 (GB2) distribution parameter
+    pp    = scalar > 0, generalized beta 2 (GB2) distribution parameter
+    qq    = scalar > 0, generalized beta 2 (GB2) distribution parameter
+
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
+        spc.beta()
+
+    OBJECTS CREATED WITHIN FUNCTION:
+    pdf_vals = (N,) vector, pdf values from generalized beta 2 (GB2)
+               distribution corresponding to xvals given parameters aa,
+               bb, pp, and qq
+
+    FILES CREATED BY THIS FUNCTION: None
+
+    RETURNS: pdf_vals
+    --------------------------------------------------------------------
+    '''
+    pdf_vals = \
+        np.float64((aa * (xvals ** (aa * pp - 1))) / ((bb ** (aa * pp)) *
+                   spc.beta(pp, qq) *
+                   ((1 + ((xvals / bb) ** aa)) ** (pp + qq))))
+
+    return pdf_vals
